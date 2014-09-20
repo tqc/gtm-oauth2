ac491f6f0ddccd403f30ec59e53c8c4eb8a34bdb
Tom Clarkson <tom@orangeguava.com>
2014-09-20 19:18:38 +1000

    include httpfetcher, update project target

--- Examples/OAuth2SampleTouch/OAuth2SampleRootViewControllerTouch.m
+++ Examples/OAuth2SampleTouch/OAuth2SampleRootViewControllerTouch.m
@@ -25,8 +25,8 @@ static NSString *const kShouldSaveInKeychainKey = @"shouldSaveInKeychain";
 static NSString *const kDailyMotionAppServiceName = @"OAuth Sample: DailyMotion";
 static NSString *const kDailyMotionServiceName = @"DailyMotion";
 
-static NSString *const kSampleClientIDKey = @"clientID";
-static NSString *const kSampleClientSecretKey = @"clientSecret";
+//static NSString *const kSampleClientIDKey = @"clientID";
+//static NSString *const kSampleClientSecretKey = @"clientSecret";
 
 @interface OAuth2SampleRootViewControllerTouch()
 - (void)viewController:(GTMOAuth2ViewControllerTouch *)viewController
